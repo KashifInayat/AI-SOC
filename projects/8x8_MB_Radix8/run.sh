d09e92df0bf9f7cc5fa9d75bc0701eac54681227
KASHIF INAYAT <26200848+KashifInayat@users.noreply.github.com>
2021-04-22 17:09:47 +0900

    Delete projects directory

--- projects/8x8_MB_Radix8/run.sh
+++
@@ -1,4 +0,0 @@
-vcs -full64 ./test1.v ../../rtl/8x8_MB_Radix_8/mb8_radix8.v -ld ld -LDFLAGS "/usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o" +v2k  -gui -sverilog
-./simv
-#+incdir+../../rtl/s_ice/ +define+SERIAL_MULTIPLIER_TEST
-#-sverilog 
